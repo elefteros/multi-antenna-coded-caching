1b25e62bcf328abcd88705355ee81471eec450bd
elefteros <58448626+elefteros@users.noreply.github.com>
2019-12-03 13:00:47 +0100

    Update timeCalculation.m

--- timeCalculation.m
+++ timeCalculation.m
@@ -1,6 +1,6 @@
 function[time] = timeCalculation(t, streams, hp, rp, power, iter)
 
-    S= (t+ streams) * log( 1+ power.*(hp'*rp)/streams)/iter;
+    S= (t+ streams) * log( 1+ power.*(hp'*rp)/streams)*iter;
     
     time=S.^(-1);
     
